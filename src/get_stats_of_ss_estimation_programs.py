67664ce93d209390aba208d7d73ebaeb8a07d45f
heartsh <heartsh@heartsh.io>
2020-06-27 07:12:53 +0900

    Produce figures of gammas vs metrics

--- src/get_stats_of_ss_estimation_programs.py
+++ src/get_stats_of_ss_estimation_programs.py
@@ -17,25 +17,35 @@ def main():
   conshomfold_ppvs = []
   conshomfold_senss = []
   conshomfold_fprs = []
+  conshomfold_f1_scores = []
+  conshomfold_mccs = []
   centroidhomfold_ppvs = []
   centroidhomfold_senss = []
   centroidhomfold_fprs = []
+  centroidhomfold_f1_scores = []
+  centroidhomfold_mccs = []
   turbofold_ppvs = []
   turbofold_senss = []
   turbofold_fprs = []
+  turbofold_f1_scores = []
+  turbofold_mccs = []
   contrafold_ppvs = []
   contrafold_senss = []
   contrafold_fprs = []
+  contrafold_f1_scores = []
+  contrafold_mccs = []
   centroidfold_ppvs = []
   centroidfold_senss = []
   centroidfold_fprs = []
+  centroidfold_f1_scores = []
+  centroidfold_mccs = []
   bpp_conshomfold_ppvs = []
   bpp_conshomfold_senss = []
   bpp_conshomfold_fprs = []
-  rnafold_ppv = rnafold_sens = rnafold_fpr = 0.
+  bpp_conshomfold_f1_scores = []
+  bpp_conshomfold_mccs = []
+  rnafold_ppv = rnafold_sens = rnafold_fpr = rnafold_f1_score = rnafold_mcc = 0.
   gammas = [2. ** i for i in range(-7, 11)]
-  conshomfold_f1_score = centroidhomfold_f1_score = turbofold_f1_score = rnafold_f1_score = 0.
-  conshomfold_mcc = centroidhomfold_mcc = turbofold_mcc = rnafold_mcc = 0.
   bpp_conshomfold_ss_dir_path = asset_dir_path + "/bpp_conshomfold"
   conshomfold_ss_dir_path = asset_dir_path + "/conshomfold"
   centroidhomfold_ss_dir_path = asset_dir_path + "/centroidhomfold"
@@ -71,9 +81,8 @@ def main():
       centroidfold_estimated_ss_dir_path = os.path.join(centroidfold_ss_dir_path, rna_fam_name)
       conshomfold_estimated_ss_file_path = os.path.join(conshomfold_estimated_ss_dir_path, "gamma=" + gamma_str + ".fa")
       conshomfold_count_params.insert(0, (conshomfold_estimated_ss_file_path, ref_sss_and_flat_sss, rna_seq_lens))
-      if gamma >= 2.:
-        bpp_conshomfold_estimated_ss_file_path = os.path.join(bpp_conshomfold_estimated_ss_dir_path, "gamma=" + gamma_str + ".fa")
-        bpp_conshomfold_count_params.insert(0, (bpp_conshomfold_estimated_ss_file_path, ref_sss_and_flat_sss, rna_seq_lens))
+      bpp_conshomfold_estimated_ss_file_path = os.path.join(bpp_conshomfold_estimated_ss_dir_path, "gamma=" + gamma_str + ".fa")
+      bpp_conshomfold_count_params.insert(0, (bpp_conshomfold_estimated_ss_file_path, ref_sss_and_flat_sss, rna_seq_lens))
       centroidhomfold_estimated_ss_file_path = os.path.join(centroidhomfold_estimated_ss_dir_path, "gamma=" + gamma_str + ".fa")
       centroidhomfold_count_params.insert(0, (centroidhomfold_estimated_ss_file_path, ref_sss_and_flat_sss, rna_seq_lens))
       turbofold_estimated_ss_file_path = os.path.join(turbofold_estimated_ss_dir_path, "gamma=" + gamma_str + ".fa")
@@ -93,18 +102,18 @@ def main():
     conshomfold_ppvs.insert(0, ppv)
     conshomfold_senss.insert(0, sens)
     conshomfold_fprs.insert(0, fpr)
-    if gamma == 1.:
-      conshomfold_f1_score = get_f1_score(ppv, sens)
-      conshomfold_mcc = get_mcc(conshomfold_tp, conshomfold_tn, conshomfold_fp, conshomfold_fn)
-    if gamma >= 2.:
-      results = pool.map(get_pos_neg_counts, bpp_conshomfold_count_params)
-      bpp_conshomfold_tp, bpp_conshomfold_tn, bpp_conshomfold_fp, bpp_conshomfold_fn = final_sum(results)
-      ppv = get_ppv(bpp_conshomfold_tp, bpp_conshomfold_fp)
-      sens = get_sens(bpp_conshomfold_tp, bpp_conshomfold_fn)
-      fpr = get_fpr(bpp_conshomfold_tn, bpp_conshomfold_fp)
-      bpp_conshomfold_ppvs.insert(0, ppv)
-      bpp_conshomfold_senss.insert(0, sens)
-      bpp_conshomfold_fprs.insert(0, fpr)
+    conshomfold_f1_scores.append(get_f1_score(ppv, sens))
+    conshomfold_mccs.append(get_mcc(conshomfold_tp, conshomfold_tn, conshomfold_fp, conshomfold_fn))
+    results = pool.map(get_pos_neg_counts, bpp_conshomfold_count_params)
+    bpp_conshomfold_tp, bpp_conshomfold_tn, bpp_conshomfold_fp, bpp_conshomfold_fn = final_sum(results)
+    ppv = get_ppv(bpp_conshomfold_tp, bpp_conshomfold_fp)
+    sens = get_sens(bpp_conshomfold_tp, bpp_conshomfold_fn)
+    fpr = get_fpr(bpp_conshomfold_tn, bpp_conshomfold_fp)
+    bpp_conshomfold_ppvs.insert(0, ppv)
+    bpp_conshomfold_senss.insert(0, sens)
+    bpp_conshomfold_fprs.insert(0, fpr)
+    bpp_conshomfold_f1_scores.append(get_f1_score(ppv, sens))
+    bpp_conshomfold_mccs.append(get_mcc(bpp_conshomfold_tp, bpp_conshomfold_tn, bpp_conshomfold_fp, bpp_conshomfold_fn))
     results = pool.map(get_pos_neg_counts, centroidhomfold_count_params)
     centroidhomfold_tp, centroidhomfold_tn, centroidhomfold_fp, centroidhomfold_fn = final_sum(results)
     ppv = get_ppv(centroidhomfold_tp, centroidhomfold_fp)
@@ -113,9 +122,8 @@ def main():
     centroidhomfold_ppvs.insert(0, ppv)
     centroidhomfold_senss.insert(0, sens)
     centroidhomfold_fprs.insert(0, fpr)
-    if gamma == 1.:
-      centroidhomfold_f1_score = get_f1_score(ppv, sens)
-      centroidhomfold_mcc = get_mcc(centroidhomfold_tp, centroidhomfold_tn, centroidhomfold_fp, centroidhomfold_fn)
+    centroidhomfold_f1_scores.append(get_f1_score(ppv, sens))
+    centroidhomfold_mccs.append(get_mcc(centroidhomfold_tp, centroidhomfold_tn, centroidhomfold_fp, centroidhomfold_fn))
     results = pool.map(get_pos_neg_counts, turbofold_count_params)
     turbofold_tp, turbofold_tn, turbofold_fp, turbofold_fn = final_sum(results)
     ppv = get_ppv(turbofold_tp, turbofold_fp)
@@ -124,9 +132,8 @@ def main():
     turbofold_ppvs.insert(0, ppv)
     turbofold_senss.insert(0, sens)
     turbofold_fprs.insert(0, fpr)
-    if gamma == 1.:
-      turbofold_f1_score = get_f1_score(ppv, sens)
-      turbofold_mcc = get_mcc(turbofold_tp, turbofold_tn, turbofold_fp, turbofold_fn)
+    turbofold_f1_scores.append(get_f1_score(ppv, sens))
+    turbofold_mccs.append(get_mcc(turbofold_tp, turbofold_tn, turbofold_fp, turbofold_fn))
     results = pool.map(get_pos_neg_counts, contrafold_count_params)
     contrafold_tp, contrafold_tn, contrafold_fp, contrafold_fn = final_sum(results)
     ppv = get_ppv(contrafold_tp, contrafold_fp)
@@ -135,9 +142,8 @@ def main():
     contrafold_ppvs.insert(0, ppv)
     contrafold_senss.insert(0, sens)
     contrafold_fprs.insert(0, fpr)
-    if gamma == 1.:
-      contrafold_f1_score = get_f1_score(ppv, sens)
-      contrafold_mcc = get_mcc(contrafold_tp, contrafold_tn, contrafold_fp, contrafold_fn)
+    contrafold_f1_scores.append(get_f1_score(ppv, sens))
+    contrafold_mccs.append(get_mcc(contrafold_tp, contrafold_tn, contrafold_fp, contrafold_fn))
     results = pool.map(get_pos_neg_counts, centroidfold_count_params)
     centroidfold_tp, centroidfold_tn, centroidfold_fp, centroidfold_fn = final_sum(results)
     ppv = get_ppv(centroidfold_tp, centroidfold_fp)
@@ -146,9 +152,8 @@ def main():
     centroidfold_ppvs.insert(0, ppv)
     centroidfold_senss.insert(0, sens)
     centroidfold_fprs.insert(0, fpr)
-    if gamma == 1.:
-      centroidfold_f1_score = get_f1_score(ppv, sens)
-      centroidfold_mcc = get_mcc(centroidfold_tp, centroidfold_tn, centroidfold_fp, centroidfold_fn)
+    centroidfold_f1_scores.append(get_f1_score(ppv, sens))
+    centroidfold_mccs.append(get_mcc(centroidfold_tp, centroidfold_tn, centroidfold_fp, centroidfold_fn))
     if gamma == 1.:
       results = pool.map(get_pos_neg_counts, rnafold_count_params)
       rnafold_tp, rnafold_tn, rnafold_fp, rnafold_fn = final_sum(results)
@@ -157,25 +162,13 @@ def main():
       rnafold_fpr = get_fpr(rnafold_tn, rnafold_fp)
       rnafold_f1_score = get_f1_score(rnafold_ppv, rnafold_sens)
       rnafold_mcc = get_mcc(rnafold_tp, rnafold_tn, rnafold_fp, rnafold_fn)
-  conshomfold_ppvs = numpy.array(conshomfold_ppvs)
-  conshomfold_senss = numpy.array(conshomfold_senss)
-  conshomfold_fprs = numpy.array(conshomfold_fprs)
-  centroidhomfold_ppvs = numpy.array(centroidhomfold_ppvs)
-  centroidhomfold_senss = numpy.array(centroidhomfold_senss)
-  centroidhomfold_fprs = numpy.array(centroidhomfold_fprs)
-  turbofold_ppvs = numpy.array(turbofold_ppvs) 
-  turbofold_senss = numpy.array(turbofold_senss)
-  turbofold_fprs = numpy.array(turbofold_fprs)
-  bpp_conshomfold_ppvs = numpy.array(bpp_conshomfold_ppvs)
-  bpp_conshomfold_senss = numpy.array(bpp_conshomfold_senss)
-  bpp_conshomfold_fprs = numpy.array(bpp_conshomfold_fprs)
-  line_1, = pyplot.plot(conshomfold_ppvs, conshomfold_senss, label = "ConsHomfold", marker = "o", linestyle = "-")
-  line_2, = pyplot.plot(turbofold_ppvs, turbofold_senss, label = "TurboFold", marker = "p", linestyle = "-")
-  line_3, = pyplot.plot(centroidhomfold_ppvs, centroidhomfold_senss, label = "CentroidHomfold", marker = "s", linestyle = "-")
-  line_4, = pyplot.plot(contrafold_ppvs, contrafold_senss, label = "CONTRAfold", marker = "h", linestyle = "-")
-  line_5, = pyplot.plot(centroidfold_ppvs, centroidfold_senss, label = "CentroidFold", marker = "v", linestyle = "-")
-  line_6, = pyplot.plot(rnafold_ppv, rnafold_sens, label = "RNAfold", marker = "d", linestyle = "-")
-  line_7, = pyplot.plot(bpp_conshomfold_ppvs, bpp_conshomfold_senss, label = "ConsHomfold (Conventional)", marker = "o", linestyle = ":")
+  line_1, = pyplot.plot(conshomfold_ppvs, conshomfold_senss, label = "ConsHomfold (Turner)", marker = "o", linestyle = "-")
+  line_2, = pyplot.plot(bpp_conshomfold_ppvs, bpp_conshomfold_senss, label = "ConsHomfold (Posterior)", marker = "o", linestyle = ":")
+  line_3, = pyplot.plot(turbofold_ppvs, turbofold_senss, label = "TurboFold", marker = "p", linestyle = "-")
+  line_4, = pyplot.plot(centroidhomfold_ppvs, centroidhomfold_senss, label = "CentroidHomfold", marker = "s", linestyle = "-")
+  line_5, = pyplot.plot(contrafold_ppvs, contrafold_senss, label = "CONTRAfold", marker = "h", linestyle = "-")
+  line_6, = pyplot.plot(centroidfold_ppvs, centroidfold_senss, label = "CentroidFold", marker = "v", linestyle = "-")
+  line_7, = pyplot.plot(rnafold_ppv, rnafold_sens, label = "RNAfold", marker = "d", linestyle = "-")
   pyplot.xlabel("Positive predictive value")
   pyplot.ylabel("Sensitivity")
   pyplot.legend(handles = [line_1, line_2, line_3, line_4, line_5, line_6, line_7], loc = "lower left")
@@ -185,26 +178,42 @@ def main():
   pyplot.tight_layout()
   pyplot.savefig(image_dir_path + "/ppvs_vs_senss_on_ss_estimation.eps", bbox_inches = "tight")
   pyplot.clf()
-  line_1, = pyplot.plot(conshomfold_fprs, conshomfold_senss, label = "ConsHomfold", marker = "o", linestyle = "-")
-  line_2, = pyplot.plot(turbofold_fprs, turbofold_senss, label = "TurboFold", marker = "p", linestyle = "-")
-  line_3, = pyplot.plot(centroidhomfold_fprs, centroidhomfold_senss, label = "CentroidHomfold", marker = "s", linestyle = "-")
-  line_4, = pyplot.plot(contrafold_fprs, contrafold_senss, label = "CONTRAfold", marker = "v", linestyle = "-")
-  line_5, = pyplot.plot(centroidfold_fprs, centroidfold_senss, label = "Centroidfold", marker = "h", linestyle = "-")
-  line_6, = pyplot.plot(rnafold_fpr, rnafold_sens, label = "RNAfold", marker = "d", linestyle = "-")
-  line_7, = pyplot.plot(bpp_conshomfold_fprs, bpp_conshomfold_senss, label = "ConsHomfold (Conventional)", marker = "o", linestyle = ":")
+  line_1, = pyplot.plot(conshomfold_fprs, conshomfold_senss, label = "ConsHomfold (Turner)", marker = "o", linestyle = "-")
+  line_2, = pyplot.plot(bpp_conshomfold_fprs, bpp_conshomfold_senss, label = "ConsHomfold (Posterior)", marker = "o", linestyle = ":")
+  line_3, = pyplot.plot(turbofold_fprs, turbofold_senss, label = "TurboFold", marker = "p", linestyle = "-")
+  line_4, = pyplot.plot(centroidhomfold_fprs, centroidhomfold_senss, label = "CentroidHomfold", marker = "s", linestyle = "-")
+  line_5, = pyplot.plot(contrafold_fprs, contrafold_senss, label = "CONTRAfold", marker = "v", linestyle = "-")
+  line_6, = pyplot.plot(centroidfold_fprs, centroidfold_senss, label = "Centroidfold", marker = "h", linestyle = "-")
+  line_7, = pyplot.plot(rnafold_fpr, rnafold_sens, label = "RNAfold", marker = "d", linestyle = "-")
   pyplot.xlabel("False positive rate")
   pyplot.ylabel("Sensitivity")
-  image_dir_path = asset_dir_path + "/images"
-  if not os.path.exists(image_dir_path):
-    os.mkdir(image_dir_path)
   pyplot.tight_layout()
   pyplot.savefig(image_dir_path + "/fprs_vs_senss_on_ss_estimation.eps", bbox_inches = "tight")
-  print("ConsHomfold's MCC & F1 score = %.3f & %.3f" %(conshomfold_mcc, conshomfold_f1_score))
-  print("TurboFold's MCC & F1 score = %.3f & %.3f" %(turbofold_mcc, turbofold_f1_score))
-  print("CentroidHomfold's MCC & F1 score = %.3f & %.3f" %(centroidhomfold_mcc, centroidhomfold_f1_score))
-  print("CONTRAfold's MCC & F1 score = %.3f & %.3f" %(contrafold_mcc, contrafold_f1_score))
-  print("Centroidfold's MCC & F1 score = %.3f & %.3f" %(centroidfold_mcc, centroidfold_f1_score))
-  print("RNAfold's MCC & F1 score = %.3f & %.3f" %(rnafold_mcc, rnafold_f1_score))
+  pyplot.clf()
+  line_1, = pyplot.plot(gammas, conshomfold_f1_scores, label = "ConsHomfold (Turner)", marker = "o", linestyle = "-")
+  line_2, = pyplot.plot(gammas, bpp_conshomfold_f1_scores, label = "ConsHomfold (Posterior)", marker = "o", linestyle = ":")
+  line_3, = pyplot.plot(gammas, turbofold_f1_scores, label = "TurboFold", marker = "p", linestyle = "-")
+  line_4, = pyplot.plot(gammas, centroidhomfold_f1_scores, label = "CentroidHomfold", marker = "s", linestyle = "-")
+  line_5, = pyplot.plot(gammas, contrafold_f1_scores, label = "CONTRAfold", marker = "v", linestyle = "-")
+  line_6, = pyplot.plot(gammas, centroidfold_f1_scores, label = "Centroidfold", marker = "h", linestyle = "-")
+  line_7, = pyplot.plot(1., rnafold_f1_score, label = "RNAfold", marker = "d", linestyle = "-")
+  pyplot.xlabel("$\gamma$")
+  pyplot.ylabel("F1 score")
+  pyplot.legend(handles = [line_1, line_2, line_3, line_4, line_5, line_6, line_7], loc = "lower right")
+  pyplot.tight_layout()
+  pyplot.savefig(image_dir_path + "/gammas_vs_f1_scores_on_ss_estimation.eps", bbox_inches = "tight")
+  pyplot.clf()
+  line_1, = pyplot.plot(gammas, conshomfold_mccs, label = "ConsHomfold (Turner)", marker = "o", linestyle = "-")
+  line_2, = pyplot.plot(gammas, bpp_conshomfold_mccs, label = "ConsHomfold (Posterior)", marker = "o", linestyle = ":")
+  line_3, = pyplot.plot(gammas, turbofold_mccs, label = "TurboFold", marker = "p", linestyle = "-")
+  line_4, = pyplot.plot(gammas, centroidhomfold_mccs, label = "CentroidHomfold", marker = "s", linestyle = "-")
+  line_5, = pyplot.plot(gammas, contrafold_mccs, label = "CONTRAfold", marker = "v", linestyle = "-")
+  line_6, = pyplot.plot(gammas, centroidfold_mccs, label = "Centroidfold", marker = "h", linestyle = "-")
+  line_7, = pyplot.plot(1., rnafold_mcc, label = "RNAfold", marker = "d", linestyle = "-")
+  pyplot.xlabel("$\gamma$")
+  pyplot.ylabel("MCC")
+  pyplot.tight_layout()
+  pyplot.savefig(image_dir_path + "/gammas_vs_mccs_on_ss_estimation.eps", bbox_inches = "tight")
 
 def get_pos_neg_counts(params):
   (estimated_ss_file_path, ref_sss_and_flat_sss, rna_seq_lens) = params
